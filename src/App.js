dffda9c4889549127e33100400a7eddf40b24e1a
Lukas Halvorsen <lukas.halvorsen@mrjaffesclass.com>
2024-02-01 14:23:59 -0800

    2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 
 import './App.css';
+import {useState} from 'react'
 import cokie from "./cookieimg.jpeg";
+import backgrnd from "./backgorund.png";
 
 function App() {
-  let counter = 0;
+  const [counter, setCounter] = useState(0);
+
+  const clickHandler = () => {
+    setCounter((prevCounter) => prevCounter+1);
+  }
+  
   return (
     <div className="App">
-      <button>{counter}</button>
+      
+      <img className = 'backrnd'
+      src = {backgrnd}> </img>
+
+
+      <li>{counter}</li>
+
+      
+      
+      
+      
+      
+      <button
+      className = 'butt'
+      onClick={clickHandler
+      }
+      > Press for cookie </button>
+      
+      
       <img 
       className='pokie' 
       src={cokie} 
       alt="help" 
-      onClick={() => {
-        clickHandler(counter)
-      }}/>
+      onClick= {clickHandler}
+      />
     </div>
   );
 }
-function clickHandler( {counter} ) {
-  counter += 1;
-}
+
 
 export default App;
